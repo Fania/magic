ec42466c5e8a2ed6dacee6719ac572620e96a4c7
Fania Ra <fania@web.de>
2020-04-21 16:23:12 +0100

    basic menu

--- meta/js/general.js
+++ meta/js/general.js
@@ -1,41 +1,53 @@
 // LOADING ICON
-loading.classList.add('show');
-window.addEventListener('load', () => loading.classList.remove('show') );
-// const loadTriggers = document.querySelectorAll('[type="submit"]');
-// loadTriggers.forEach(lt => 
-//   lt.addEventListener('click', () => loadDiv.classList.add('show')));
+const loadTriggers = document.querySelectorAll('nav a');
+loadTriggers.forEach(lt => 
+  lt.addEventListener('click', () => loading.classList.add('show') ) );
 
 
 
 
 
 // DISPLAY DATA
-async function displaySVGs(target) {
-  console.log(`Showing ${target}`);
+async function displaySVGs(target,style) {
+  console.log(`Showing ${target} in ${style} style`);
   squares.innerHTML = '';
   const rawData = await fetch(`http://localhost:3000/${target}`);
   const data = await rawData.json();
   for (let i in data.rows) {
     const elem = data.rows[i].doc;
-    squares.insertAdjacentHTML('beforeend',elem.quadvertex.svg);
+    squares.insertAdjacentHTML('beforeend',elem[style].svg);
   }
-  
+  loading.classList.remove('show');
 }
-
-
 
 uniquesMenu.addEventListener('click', () => {
   uniquesMenu.classList.add('active');
   unfilteredMenu.classList.remove('active');
-  displaySVGs('data/4/unique');
+  const currentStyle = document.querySelector('[name="displayStyle"]:checked').id;
+  displaySVGs('data/4/unique',currentStyle);
   event.preventDefault();
 });
 unfilteredMenu.addEventListener('click', () => {
   uniquesMenu.classList.remove('active');
   unfilteredMenu.classList.add('active');
-  displaySVGs('data/4/all');
+  const currentStyle = document.querySelector('[name="displayStyle"]:checked').id;
+  displaySVGs('data/4/all',currentStyle);
   event.preventDefault();
 });
 
 // default
-displaySVGs('data/4/unique');
+displaySVGs('data/4/unique','quadvertex');
+
+
+
+
+
+
+// RADIO STYLES
+
+const displayStyle = document.getElementsByName('displayStyle');
+displayStyle.forEach( dStyle => {
+  dStyle.addEventListener("change", (ev) => { 
+    displaySVGs('data/4/unique',ev.target.id);
+  })
+})
