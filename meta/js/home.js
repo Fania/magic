b2478a6f39d554eea8252acc84c6388686a55c8a
Fania Everitt <me@fania.eu>
2020-11-07 10:25:09 +0000

    more svg tag fixes

--- meta/js/home.js
+++ meta/js/home.js
@@ -3,7 +3,7 @@
 navigator.serviceWorker.register('sw.js');
 
 
-const CACHE = 'magic-v2.2.3';
+const CACHE = 'magic-v2.2.4';
 
 
 
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const cacheName = 'magic-v2.2.3';
+const cacheName = 'magic-v2.2.4';
 
 
 const precacheResources = [
