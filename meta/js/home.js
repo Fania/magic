6ad2ef55cc4d6a421979e8f36c1cacfacd7948d3
Fania Everitt <me@fania.eu>
2023-01-26 15:38:03 +0000

    test bumbing from pc

--- meta/js/home.js
+++ meta/js/home.js
@@ -3,7 +3,7 @@
 navigator.serviceWorker.register('sw.js');
 
 
-const CACHE = 'magic-v2.5.23';
+const CACHE = 'magic-v2.5.24';
 
 
 const pause = document.getElementById('pause');
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const test = require('./lib/tests.js')
 
 // START THE SERVER
 app.listen(3001, () => {
-	console.log('Magic Squares Version 2.5.23')
+	console.log('Magic Squares Version 2.5.24')
 	console.log('Running on http://localhost:3001')
 })
 
